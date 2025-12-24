f22810cca3a6b570b5af8f51545cf83559401b35
DaniloBMike <danilo7re@gmail.com>
2025-12-24 18:55:11 -0300

    update java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 public class Main {
     public static void main (String [] args) {
-        int Age = 27;
-        double incomeMonthly = 25.500;
-        char Sex = ('M');
-        boolean Emplyer = 
 
+        int Age = 30;
+        double incomeMonthly = 50.000;
+        char Sex = ('M');
+        boolean employerJava = true;
+        long quantityofCats = 15L;
+
+        System.out.println("Age " + Age);
+        System.out.println("income monthly: " + incomeMonthly);
+        System.out.println("Sex: " + Sex);
+        System.out.println("employer Java: " + employerJava);
+        System.out.println("quantity of Cats: " + quantityofCats);
 
 
     }
