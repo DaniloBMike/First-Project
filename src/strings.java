491a715b2dc9a6972ad567c327585f9eb48aad76
DaniloBMike <danilo7re@gmail.com>
2025-12-26 22:53:33 -0300

    strings

---
+++ src/strings.java
@@ -0,0 +1,14 @@
+public class strings {
+    public static void main (String[] args) {
+
+        String name = "Danilo";
+        int numbersOfCaracters = name.length();
+        String subName = "Bernardo";
+
+
+        System.out.println("Your name = " + name + subName);
+        System.out.println("Your name have = " + numbersOfCaracters + " Caracters");
+        System.out.println("Your Subname = " + subName);
+
+    }
+}
