7811213baad0354c9a5ad9cd0536419887264aff
DaniloBMike <danilo7re@gmail.com>
2025-12-28 22:06:30 -0300

    Update  java Strings

--- src/strings.java
+++ src/strings.java
@@ -3,12 +3,16 @@ public class strings {
 
         String name = "Danilo";
         int numbersOfCaracters = name.length();
-        String subName = "Bernardo";
+        String subName = " Bernardo Alves";
+        boolean same = name.equals(subName);
+        String nameComplete = name.concat(subName);
 
 
         System.out.println("Your name = " + name + subName);
         System.out.println("Your name have = " + numbersOfCaracters + " Caracters");
         System.out.println("Your Subname = " + subName);
+        System.out.println("They are equals = " + same);
+        System.out.println("Complete Name = " +nameComplete);
 
     }
 }
