a0a15de6e70e9b7422e5add4cc35fcabcc481592
DaniloBMike <danilo7re@gmail.com>
2026-01-07 22:24:57 -0300

    finishing the scanner exercise

--- src/scanner.java
+++ src/scanner.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class scanner {
     public static void main (String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write your full name here: ");
+        String name = scanner.nextLine();
+        System.out.println("Name:" + name );
+
+        System.out.println("Put your age here: ");
+        int age = scanner.nextInt();
+        System.out.println("Age:" + age);
+
+        System.out.println("You are employed?" );
+        boolean employed = scanner.nextBoolean();
+        System.out.println("Employed:" + employed);
+
+        System.out.println("Name: " + name + " age: " + age + " Employed? " + employed);
+
 
     }
 }
