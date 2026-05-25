b5cd3002585b96e1369e6f0286a2298f6e88492d
DaniloBMike <danilo7re@gmail.com>
2026-05-25 18:46:12 -0300

    update

--- src/house.java
+++ src/house.java
@@ -3,16 +3,7 @@ public class house {
 
         houseplan house = new houseplan();
 
-        house.homeSize = 95;
-        house.roomQuantity = 3;
-        house.bathroomQuantity = 2;
-        house.restRoom = 1;
-        house.kitchen = 1;
-        house.garageSpace = 2;
-        house.livingRoom = 1;
-        house.colorOutside = "light blue";
-        house.colorInside = "white and brow";
-        house.colorGarage = "light green";
+        h
 
 
         house.building();
--- src/houseplan.java
+++ src/houseplan.java
@@ -1,44 +1,37 @@
 public class houseplan {
 
+    //Attributes
 
-    // The plan base for the house
-
-    int homeSize;
+    int houseSize;
     int roomQuantity;
     int bathroomQuantity;
-    int restRoom;
-    int kitchen;
-    int livingRoom;
+    String insideColor;
+    String outsideColor;
+    String material;
+    String restroom;
+    String backyard;
     int garageSpace;
+    boolean kitchen;
 
-    // The plan style base for house
 
-    String colorOutside;
-    String colorInside;
-    String colorGarage;
+    //Method
 
-    public void building () {
+    public void buildingHouse(){
 
-        System.out.println("The home size of is : " + homeSize);
-        System.out.println("The number of rooms is : " + roomQuantity);
-        System.out.println("The number of bathrooms is : " + bathroomQuantity);
-        System.out.println("The number of restroom is : " + restRoom);
-        System.out.println("The number of kitchen is : " + kitchen);
-        System.out.println("The number of living room is : " + livingRoom);
-        System.out.println("The number of garages is : " + garageSpace);
-
-    }
-
-    public void colorsHouse () {
-
-        System.out.println(" The color of the outside house is : " + colorOutside);
-        System.out.println(" The color of inside house is : " + colorInside);
-        System.out.println(" The color of garage is : " + colorGarage);
+        System.out.println(" The total size of the house is : " + houseSize);
+        System.out.println(" The room quantity is : " + roomQuantity);
+        System.out.println(" The bathroom quantity is : " + bathroomQuantity);
+        System.out.println(" The inside color is : " + insideColor);
+        System.out.println(" The outside color is : " + outsideColor);
+        System.out.println(" The material used is : " + material);
+        System.out.println(" The quantity of restroom is : " + restroom);
+        System.out.println(" The quantity of backyard is : " + backyard);
+        System.out.println(" How many spaces there are at the garage? " + garageSpace);
+        System.out.println(" There are kitchen in the house? " + kitchen);
 
 
     }
 
 
 
-
-    }
+}
