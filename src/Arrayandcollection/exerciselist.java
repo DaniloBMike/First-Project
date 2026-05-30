8715545878ec8527e6e9630dfa3c69dfaa6c95ea
DaniloBMike <danilo7re@gmail.com>
2026-05-30 18:33:28 -0300

    Moved to package list
    Just to moved to another directory

---
+++ src/Arrayandcollection/exerciselist.java
@@ -0,0 +1,62 @@
+package Arrayandcollection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class exerciselist {
+    public static void main (String[] args) {
+
+        List<String> students = new ArrayList<>();
+
+        students.add("Ana");
+        students.add("Bruno");
+        students.add("Carlos");
+        students.add("Diana");
+
+        students.add(2, "Eduardo");
+
+        students.remove(1);
+
+        int studant = students.size();
+        System.out.println(" The size is : " + studant);
+
+        String nameStudent = students.get(2);
+        System.out.println(" The third student is : " + nameStudent);
+
+        for (String allStudents : students) {
+            System.out.println(allStudents);
+        }
+
+        boolean empty = students.isEmpty();
+        System.out.println("The list is empty ? " + empty);
+
+        boolean contein = students.contains("Diana");
+        System.out.println("There are Diana in the list ? " + contein);
+
+        students.forEach(school -> System.out.println(school));
+
+
+        List<String> team = new ArrayList<>();
+
+        team.add("Ana");
+        team.add("Bob");
+        team.add("Carlos");
+        team.add("Diana");
+        team.add("Eduardo");
+
+        team.remove(2);
+
+        team.add(1, "Felipe");
+
+
+        int sizeTeam = team.size();
+        System.out.println(" The size is  : " + sizeTeam);
+
+        for (String allnames : team){
+            System.out.println(allnames);
+        }
+
+
+    }
+
+}
