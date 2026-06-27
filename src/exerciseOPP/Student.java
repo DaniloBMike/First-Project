20209e27f0d5a904292058e515202ac09cbf1101
DaniloBMike <danilo7re@gmail.com>
2026-06-27 18:45:45 -0300

    update
    updating the exercise

--- src/exerciseOPP/Student.java
+++ src/exerciseOPP/Student.java
@@ -2,7 +2,7 @@ package exerciseOPP;
 
 public class Student {
 
-    String name;
+    String name1, name2;
     double grade1;
     double grade2;
 
@@ -13,13 +13,14 @@ public class Student {
 
     public void Result() {
         double resultAverage = Average();
-        System.out.println( " The average result is " + resultAverage );
+        System.out.println( " The average result of Danilo's grade is " + resultAverage );
         if ( resultAverage >= 7.0) {
-            System.out.println( " The student Danilo pass the exam " + resultAverage );
+            System.out.println( name1 + " Approved " );
         }
         else {
-            System.out.println(" The student Danilo do not pass the exam " + resultAverage);
+            System.out.println( name2 + " Failed ");
         }
+
 
     }
 }
--- src/exerciseOPP/StudentGrades.java
+++ src/exerciseOPP/StudentGrades.java
@@ -5,13 +5,13 @@ public class StudentGrades {
 
         Student studentGrades = new Student();
 
-        studentGrades.name = " Danilo ";
+        studentGrades.name1 = " Danilo ";
         studentGrades.grade1 = 6.5;
         studentGrades.grade2 = 8.3;
 
         Student studentGrades2 = new Student();
 
-        studentGrades2.name = " Vanessa ";
+        studentGrades2.name2 = " Vanessa ";
         studentGrades2.grade1 = 7.5;
         studentGrades2.grade2 = 5.3;
 
