027559be9002e776d1512b914a8551e140003231
DaniloBMike <danilo7re@gmail.com>
2026-06-29 18:43:38 -0300

    Created a new OOP exercise
    New OOP exercise

---
+++ src/exerciseOPP/rectangle.java
@@ -0,0 +1,20 @@
+package exerciseOPP;
+
+public class rectangle {
+
+    double width;
+    double height;
+
+
+    public double area() {
+        return width * height;
+    }
+    public double perimeter() {
+        return 2 * ( width + height );
+    }
+    public double describe() {
+        
+    }
+
+
+}
