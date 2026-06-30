55d50c8e784fa97b2730a0f4f45c2255e576246b
DaniloBMike <danilo7re@gmail.com>
2026-06-30 18:56:31 -0300

    Created two new object called result

---
+++ src/exerciseOPP/ResultParameters.java
@@ -0,0 +1,13 @@
+package exerciseOPP;
+
+public class ResultParameters {
+    public static void main(String[] args) {
+
+        rectangle result = new rectangle();
+
+        result.width = 50.50;
+        result.height = 35.35;
+
+
+    }
+}
