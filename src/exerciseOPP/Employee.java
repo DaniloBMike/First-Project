ffbd85378aeef060a951a21a34cb8449426bd063
DaniloBMike <danilo7re@gmail.com>
2026-07-03 18:45:39 -0300

    Created a new exercise
    New exercise called Employee

---
+++ src/exerciseOPP/Employee.java
@@ -0,0 +1,11 @@
+package exerciseOPP;
+
+public class Employee {
+
+    String Profession;
+    String name;
+    double salary;
+    int hoursWorked;
+
+
+}
---
+++ src/exerciseOPP/Workers.java
@@ -0,0 +1,13 @@
+package exerciseOPP;
+
+public class Workers {
+    public static void main ( String[] args) {
+
+
+        Workers employeesData = new Workers();
+
+
+
+    }
+
+}
