8555dce9663cd73ee8017683d63ca46ec7da728e
DaniloBMike <danilo7re@gmail.com>
2026-07-19 18:48:55 -0300

    Created a new opp exercise about method statics and instance

---
+++ src/exerciseOPP/Staticmethod.java
@@ -0,0 +1,18 @@
+package exerciseOPP;
+
+public class Staticmethod {
+
+    public static int multiplying ( int a, int b ) {
+
+        return a + b;
+
+    }
+
+    public static String changeNumber ( Integer a ) {
+
+        return a.toString();
+
+    }
+
+
+}
