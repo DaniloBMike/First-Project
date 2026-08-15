95df024339074f92df70d8c88d5c5e65e95da4a8
DaniloBMike <danilo7re@gmail.com>
2026-08-15 19:02:44 -0300

    Created Employee opp class
    New opp class exercise created called Employee

---
+++ src/exerciseOPP/Construction/Employee.java
@@ -0,0 +1,17 @@
+package exerciseOPP.Construction;
+
+public class Employee {
+
+        //Employee 01
+
+        String name;
+        int age;
+        int salary;
+        int hoursWorked;
+
+        public Employee (String name, int age, int salary, int hoursWorked) {
+
+            this.name = name
+
+        }
+}
